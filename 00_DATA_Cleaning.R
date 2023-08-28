333be5cf97dfb79ab014fed690d4a74ca98bfedb
Pawel Waryszak <pawel.waryszak.com>
2023-08-28 17:56:36 +1000

    Cleaned the Phyla Taxa and Ecosystem data and ran NMDS on them.

--- 00_DATA_Cleaning.R
+++ 00_DATA_Cleaning.R
@@ -1,5 +1,4 @@
 #RAW DATA Cleaning:=======
-
 #Load Libraries and Notes:
 library(tidyverse)
 library(readxl)
@@ -12,6 +11,7 @@ library(vegan)
 #Pawel continues to clean taxa names in "Taxa_DATA_ALL_EXTRACTED.xlsx" file:
 #Link3GDRIVE: https://docs.google.com/spreadsheets/d/1yICgLcTXEYdZul7Hto4U4CA0xgr34vNO/edit?usp=drive_link&ouid=100881671914675049849&rtpof=true&sd=true
 
+#LOAD TAXA data
 taxa <- read_excel("Taxa_DATA_ALL_EXTRACTED.xlsx", sheet = "TaxaDATA_CLEAN") #Get dataset cleaned (21-Aug-2023) on Google Drive here:
 df <- taxa %>% mutate_if(is.character, as.factor) #convert all columns from character to factor for further manipulations
 names(df)
@@ -25,74 +25,223 @@ names(df)
 #[8] "Microbial_taxa_extracted_clean"  
 # From BlueBug01" to BlueBug65" - this column "Microbial_taxa_extracted_clean" split by comma into single taxa names
 
+
+#Turn taxa into taxa_long for matching with phyla next:
+taxa_long <- gather(df, key = "BlueBug", value = "SeekPhyla", BlueBug01:BlueBug65, na.rm=T) #Turning 65 BluBug columns into one "SeekPhyla" column
+names(taxa_long)
+dim(taxa_long) #2220   13
+
+
 #LOAD Phyla dataset:
 phyla1 <- read_excel("Taxa_DATA_ALL_EXTRACTED.xlsx", sheet = "PhylaFromReview") #Get clean CSM
+names(phyla1)#"Phyla" "Group"
+P1 <- as.data.frame( phyla1[,1])  #P1 data from our literature review contains 55 phylas.
+P1
+
 phyla2 <- read_excel("Ludwig2021_PHYLA_Dataset_LTP_06_2022.xlsx", sheet = "Table S1 number of taxa in LTP",skip=2) #Get phyla from paper by Ludwig 2021:
-P2 <- phyla2[,1] #PHyla from Ludwig2021 had no hits in our data. I had to combine it with P1. For example Proteobacteria
-colnames(P2)[1]<-"Phyla"
-P1 <- phyla1[,1] #P1 data from our literature review.
-P1P2 <- rbind (P1,P2)#Hybrid phyla list, combining Ludwig2021 and our own dataset.
+P2 <- as.data.frame(phyla2[1:39,1] )  #Total of 39 Phylas from Ludwig2021 had no hits in our data. I combined it with phyla present in our reveiw. For example Proteobacteria not present in Ludwig2021
+colnames(P2)[ 1]<-"Phyla" #Rename column to match P1 for rbind function
+P2
 
-
-taxa_long <- gather(df, key = "BlueBug", value = "SeekPhyla", BlueBug01:BlueBug65, na.rm=T) #Turning 64 BluBug columns into "SeekPhyla" column
-View(taxa_long)
-dim(taxa_long) #2887   10
+P1P2 <- rbind (P1,P2)# combining Ludwig2021 and our own phyla-dataset.
+P1P2
 
 
 taxa2 <- taxa_long %>%
-  mutate(P = if_else(SeekPhyla %in% P1P2 $ Phyla, SeekPhyla, "NOPE"))##Fill new "P" with Phyla if present in "SeekPhyla" column:
-dim(taxa2)
+  mutate(Phyla_Hits = if_else(SeekPhyla %in% P1P2 $ Phyla, SeekPhyla, "NONE"))##Fill new "P" with Phyla if present in "SeekPhyla" column:
+dim(taxa2) #2220   14
 
 
-taxa2spread <- taxa2 %>% group_by(P) %>%
-  summarise(n = n())
-  
-View(taxa2spread)
+taxa2spread <- taxa2 %>% group_by(Phyla_Hits) %>%  summarise(n = n())
 head(taxa2spread) 
-# P                  n
-# Acidobacteria     88
 
+
+#CREATE TABLE S1 PHYLA_HITS in WORD using "taxa2spread":=========
+library(officer)
+library(flextable)
+
+ft <- flextable(data = taxa2spread) %>% theme_zebra %>% autofit # Create flextable object
+
+ft# See flextable in RStudio viewer
+
+tmp <- tempfile(fileext = ".docx") # Create a temp file
+
+read_docx() %>%   body_add_flextable(ft) %>%   print(target = tmp) # Create a docx file:
+
+browseURL(tmp) # open word document. SAVED as Phyla_Hits.docx Table S1
+#Link3GDRIVE: https://docs.google.com/spreadsheets/d/1yICgLcTXEYdZul7Hto4U4CA0xgr34vNO/edit?usp=drive_link&ouid=100881671914675049849&rtpof=true&sd=true
+
+
+
+
+#Sum-up Phyla-hits==========
 yes<- taxa2spread %>% filter(P!="NOPE")
 yes_sum <- sum(yes$n)
 
 no <- taxa2spread %>% filter(P=="NOPE")
 no_sum <- sum(no$n)
+
 NOPE <- no_sum/ (yes_sum + no_sum) *100
 round(NOPE,0) #60% of NO PHYLA info
 
 
-################
-#Stopped here on 21-Aug-2023
-####################
-#Merge treat and community matrix by StudyID and run NMDS
 
+#CLEAN VEG data off singletons========
 
-
-
-
-
-#NMDS====
-#Get species matric (Phyla as columns)
+#Get species matrix (Phyla as columns)
 taxa_nmds <- taxa2 %>% 
-  group_by(StudyID_Cov, P) %>%
+  group_by(StudyID_Cov, Phyla_Hits) %>%
   summarise(n = n()) %>%
-  spread(P,n, fill=0)
+  spread(Phyla_Hits,n, fill=0)
+
+dim(taxa_nmds)#341  66
 
 #Get ecosystem as grouping factor:
-eco <- read_excel("DataAnalysis_BlueBugs2022.xlsx", sheet = "clean_data") #Get  origin dataset  to get ecosystem data
+eco <- read_excel("DataAnalysis_BlueBugs2022.xlsx", sheet = "clean_data") %>% #Get  origin dataset  to get ecosystem data
+                 filter(Ecosystem == "mangrove" | Ecosystem == "saltmarsh"|Ecosystem == "seagrass") #filter out the couple of records with mixed records: mix, soil, NA values 
+               
+dim(eco)
 
-community_matrix <- taxa_nmds[ ,2:65] #Get the community matrix of Phyla
-View(community_matrix)
+#CLEAN THE ROWS:
+taxa_nmds$row_sum <- rowSums(taxa_nmds[ , 2:65], na.rm=TRUE)#2:65 are columns with phyla from Acidobacteria to Zygomycota)
+clean_rows <- taxa_nmds %>%  filter(row_sum >= 1) %>%# Filter out rows and columns with sums smaller than 1
+               select(-row_sum)
+dim(taxa_nmds) #341  67 n of rows in each dataset
+dim(clean_rows) # 341  66
 
-#Get
-treat <- as.data.frame(eco$Ecosystem) #Get the ecosystem as grouping variable.
+#CLEAN THE COLUMNS:
+column_sums <- colSums(taxa_nmds[, -1]) # Calculate phyla column sums
+column_sums_data <- data.frame(StudyID_Cov = "Sum", taxa_nmds [1, -1] * 0 + column_sums)#  Create a data frame with the sum row
 
-#Run NMDS:
-example_NMDS <- metaMDS(community_matrix,k=2,trymax=10)
+# Combine the column_sums_data with the original dataset taxa_nmds
+sums <- rbind(taxa_nmds, column_sums_data)
+dim(taxa_nmds) # 341  67
+dim(clean_rows) #341  66
 
-#Plot NMDS:
-ordiplot(example_NMDS)
-ordihull(example_NMDS,groups=treat,draw="polygon",col="grey90",label=F)
-orditorp(example_NMDS,display="species",col="red",air=0.01)
-orditorp(example_NMDS,display="sites",col=c(rep("green",5),rep("blue",5)),
-   air=0.01,cex=1.25)
+#ID Singleton columns:
+occur.cols <- apply(taxa_nmds[,-1],2,sum)#sum species occurrences in each column
+table(occur.cols) #no zero abundances
+
+#remove 0-sum  columns if any:
+good.matrix <- taxa_nmds[,-1][ , ! occur.cols <= 0  ] #removing all 0-sum  columns
+names(good.matrix) #341  66 = there were no zero columns
+
+clean_columns <- good.matrix %>% select(-row_sum) #Remove that Row_sum column before NMDS
+
+StudyID_Cov_community_matrix <- cbind(clean_rows[,"StudyID_Cov"], clean_columns)
+DATA<- left_join(StudyID_Cov_community_matrix, eco, by = "StudyID_Cov")
+
+View(DATA)
+
+#Get ecosystems
+treat <- as.data.frame(DATA$Ecosystem)#Get the ecosystem as grouping variable.
+colnames(treat)[1] <- "region"
+
+#NMDS:=============
+community_matrix <- clean_columns
+veg.env <- cbind(community_matrix, treat)
+
+MDS <- metaMDS(community_matrix,k=2,trymax=10,, distance = "bray")
+MDS$stress # Stress = 0.0005720224
+
+coordinates<-as.data.frame(MDS$points[,1:2])#site scores or:
+veg.nmds<-as.data.frame(cbind(coordinates, veg.env))
+names(veg.nmds)
+
+region_data <- filter(veg.nmds, region == "mangrove" |region == "saltmarsh"|region=="seagrass") %>%
+  filter( MDS1 < 5) #one big outlier in MDS
+  
+dim(veg.nmds); dim(region_data)            
+
+#Draw a hull:
+grp.a <- region_data[region_data$region == "mangrove", ][chull(region_data[region_data$region =="mangrove", c("MDS1", "MDS2")]), ]
+grp.b <- region_data[region_data$region == "saltmarsh", ][chull(region_data[region_data$region =="saltmarsh", c("MDS1", "MDS2")]), ]
+grp.c <- region_data[region_data$region == "seagrass", ][chull(region_data[region_data$region =="seagrass", c("MDS1", "MDS2")]), ]
+
+hull.data <- rbind(grp.a, grp.b,grp.c)  #combine grp.a and grp.b
+
+reg_plot <- ggplot() + 
+  geom_polygon(data=hull.data,aes(x=MDS1,y=MDS2,fill=region, group=region),alpha=0.30) + # add the convex hulls
+  geom_point(data=region_data ,aes(x=MDS1,y=MDS2,shape=region,colour=region),size=4)+ # add the point markers
+  #scale_colour_manual(values=c("mangrove" = "red", "seagrass" = "blue", saltmarsh = "brown")) +
+  labs(x = "NMDS 1",y="NMDS 2", colour = "", shape = "")+
+  guides(fill = FALSE, size = FALSE)+
+  #coord_equal() +
+  theme_bw() +
+  theme(#axis.text.x = element_blank(),  # remove x-axis text
+    #axis.text.y = element_blank(), # remove y-axis text
+    #axis.ticks = element_blank(),  # remove axis ticks
+    #axis.title.x = element_text(size=18), # remove x-axis labels
+    axis.title.y = element_text(size=18),
+    axis.title.x = element_text(size=18),
+    legend.position = "top",
+    legend.text = element_text(size=18),
+    legend.title  = element_text(size=18),
+    panel.background = element_blank(), 
+    panel.grid.major = element_blank(),  #remove major-grid labels
+    panel.grid.minor = element_blank(),  #remove minor-grid labels
+    plot.background = element_blank())  
+
+reg_plot
+
+ggsave(reg_plot,bg = "white",width = 12, height = 7, file = "NMDS_PLOT_EcossytemsPhyla_40PercentOfDATA.png")
+
+
+
+
+#NMDS ALL DATA:=============
+community_matrix <- clean_columns
+veg.env <- cbind(community_matrix, treat) #Combininig ecosystem with phyla matrix
+
+MDS <- metaMDS(community_matrix,k=2,trymax=10, distance = "bray")
+MDS$stress # Stress = 0.0005720224
+
+coordinates<-as.data.frame(MDS$points[,1:2])#site scores or:
+veg.nmds<-as.data.frame(cbind(coordinates, veg.env))
+region_data <- filter(veg.nmds, region == "mangrove" |region == "saltmarsh"|region=="seagrass") 
+
+#Draw a hull:
+grp.a <- region_data[region_data$region == "mangrove", ][chull(region_data[region_data$region =="mangrove", c("MDS1", "MDS2")]), ]
+grp.b <- region_data[region_data$region == "saltmarsh", ][chull(region_data[region_data$region =="saltmarsh", c("MDS1", "MDS2")]), ]
+grp.c <- region_data[region_data$region == "seagrass", ][chull(region_data[region_data$region =="seagrass", c("MDS1", "MDS2")]), ]
+
+hull.data <- rbind(grp.a, grp.b,grp.c)  #combine grp.a and grp.b
+
+reg_plot2 <- ggplot() + 
+  geom_polygon(data=hull.data,aes(x=MDS1,y=MDS2,fill=region, group=region),alpha=0.30) + # add the convex hulls
+  geom_point(data=region_data ,aes(x=MDS1,y=MDS2,shape=region,colour=region),size=4)+ # add the point markers
+  #scale_colour_manual(values=c("mangrove" = "red", "seagrass" = "blue", saltmarsh = "brown")) +
+  labs(x = "NMDS 1",y="NMDS 2", colour = "", shape = "")+
+  guides(fill = FALSE, size = FALSE)+
+  #coord_equal() +
+  theme_bw() +
+  theme(#axis.text.x = element_blank(),  # remove x-axis text
+    #axis.text.y = element_blank(), # remove y-axis text
+    #axis.ticks = element_blank(),  # remove axis ticks
+    #axis.title.x = element_text(size=18), # remove x-axis labels
+    axis.title.y = element_text(size=18),
+    axis.title.x = element_text(size=18),
+    legend.position = "top",
+    legend.text = element_text(size=18),
+    legend.title  = element_text(size=18),
+    panel.background = element_blank(), 
+    panel.grid.major = element_blank(),  #remove major-grid labels
+    panel.grid.minor = element_blank(),  #remove minor-grid labels
+    plot.background = element_blank())  
+
+reg_plot2
+
+
+ggsave(reg_plot2,bg = "white",width = 12, height = 7, file = "NMDS_OUTLIERS_PLOT_EcossytemsPhyla_40PercentOfDATA.png")
+
+
+################
+#Stopped here on 28-Aug-2023
+####################
+
+
+
+
+
+
+
